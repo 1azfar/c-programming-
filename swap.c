94e4f690e50faa83575d7b742b48cdb5569bb3c7
Azfar <azfarnajmi06@gmail.com>
2026-09-18 22:32:10 +0530

    Add swap program

---
+++ swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, temp;
+
+    printf("Enter first number (a):");
+    scanf("%d", &a);
+    printf("Enter second number (b):");
+    scanf("%d", &b);
+
+    printf("\n Before swapping a = %d, b = %d", a, b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("\n After swapping a = %d, b = %d", a, b);
+
+    return 0;
+}
